458435aafd841f09171f5c7484e3f7c60db1de24
Shravan Bobade <shravanbobade@Shravans-Laptop.local>
2024-07-14 00:06:18 +0530

    Succesfully Added Database and Created newUser Registration

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,21 +1,66 @@
 import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js"
+import { Console } from "console";
 
 export const register = async (req,res) =>{
     //db operation
     const { username , email , password } = req.body;
 
-    //hash the password
+    try{
+        //hash the password
 
-    const hashedPassword = await bcrypt.hash(password , 10);
-    console.log(hashedPassword);
+        const hashedPassword = await bcrypt.hash(password , 10);
+
+        //create new user and send to DB
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword,
+            },
+        })
+
+        console.log(newUser);
+
+        // Ensure only one response is sent
+        if (!res.headersSent) {
+          res.status(201).json({ message: "User Creates Succesfully"});
+        }
+      } catch (error) {
+        console.error('Error during user registration:', error); // Log the error for debugging
+        if (!res.headersSent) {
+          res.status(500).json({ message: "Failed to Create User"});
+        }
+      } 
     
-    //create new user and send to DB
+};
 
-
-}
-
-export const login = (req,res) =>{
+export const login = async (req,res) =>{
     //db operation
+    const { username , password } = req.body;
+
+    try{
+      //check if user exist
+      const user = await prisma.user.findUnique({
+        where: { username }
+      })
+
+      if(!user) return res.status(401).json({ message: "Invalid Credentials!" });
+
+      //check if password is correct
+      const isPasswordValid = await bcrypt.compare(password , user.password);
+  
+      if(!isPasswordValid) return res.status(401).json({ message: "Invalid Credentials!" });
+
+      //generate cookie token and send it to user
+      res.setHeader("Set-Cookie" ,"test=" + "myValue").json("success");
+
+    } catch (error) {
+      console.error('Error during user registration:', error); // Log the error for debugging
+      if (!res.headersSent) {
+        res.status(500).json({ message: "Failed to Create User"});
+      }
+    } 
 }
 
 export const logout = (req,res) =>{
