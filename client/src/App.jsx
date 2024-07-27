6d6539b6d3d67f06d83af2054661ad6c4a2dc411
Shravan Bobade <shravanbobade@Shravans-Laptop.local>
2024-07-27 11:18:22 +0530

    added fetching data using react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Login from "./routes/login/login";
 import Register from "./routes/register/register";
 import ProfileUpdatePage from "./routes/profileUpdatePage/profileUpdatePage";
 import NewPostPage from "./routes/newPostPage/newPostPage.jsx";
+import { singlePageLoader } from "./lib/loader.js";
 
 function App() {
   const router = createBrowserRouter([
@@ -26,6 +27,7 @@ function App() {
         {
           path: "/:id",
           element: <SinglePage />,
+          loader: singlePageLoader,
         },
         {
           path: "/login",
